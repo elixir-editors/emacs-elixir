85babab6d297f771b637449e30b260912c157308
Jonathan J Hunt <hunt@braincorporation.com>
2013-06-02 17:37:14 -0700

    Make sure tabs are replace with spaces regardless of the global emacs mode.

--- elixir-mode.el
+++ elixir-mode.el
@@ -336,6 +336,7 @@
   (setq mode-name "Elixir")
   (set (make-local-variable 'comment-start) "# ")
   (set (make-local-variable 'comment-end) "")
+  (set (make-local-variable 'indent-tabs-mode) nil)
   (set (make-variable-buffer-local 'tab-width) elixir-basic-offset)
   (set (make-variable-buffer-local 'default-tab-width) elixir-basic-offset)
   (smie-setup elixir-smie-grammar 'verbose-elixir-smie-rules ; 'elixir-smie-rules
