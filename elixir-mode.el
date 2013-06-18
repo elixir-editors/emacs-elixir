da68159336c17f2e695bee313c1be862e2c9e3aa
Samuel Tonini <tonini.samuel@gmail.com>
2013-06-18 21:10:19 +0200

    fix *.elixir file ending elixir-mode autload

--- elixir-mode.el
+++ elixir-mode.el
@@ -386,7 +386,7 @@
 
 ;;;###autoload
 (progn
-  (add-to-list 'auto-mode-alist '("\\.elixir$'" . elixir-mode))
+  (add-to-list 'auto-mode-alist '("\\.elixir$" . elixir-mode))
   (add-to-list 'auto-mode-alist '("\\.ex$" . elixir-mode))
   (add-to-list 'auto-mode-alist '("\\.exs$" . elixir-mode)))
 
