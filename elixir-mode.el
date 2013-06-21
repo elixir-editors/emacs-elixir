48f610747072ed70d71acffc706dff6bb1c41728
Samuel Tonini <tonini.samuel@gmail.com>
2013-06-21 23:10:00 +0200

    prepare 1.2.0

--- elixir-mode.el
+++ elixir-mode.el
@@ -8,7 +8,7 @@
 ;; URL: https://github.com/elixir-lang/emacs-elixir
 ;; Created: Mon Nov 7 2011
 ;; Keywords: languages elixir
-;; Version: 1.1.0
+;; Version: 1.2.0
 
 ;; This file is not a part of GNU Emacs.
 
@@ -109,7 +109,7 @@
 
 (require 'elixir-smie)  ; syntax and indentation support
 
-(defvar elixir-mode--version "1.1.0")
+(defvar elixir-mode--version "1.2.0")
 
 (defvar elixir-mode--website-url
   "http://elixir-lang.org")
