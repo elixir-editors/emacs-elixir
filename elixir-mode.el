6619aa828a80dded554c690e664e7011997df731
Matt DeBoard <matt.deboard@gmail.com>
2014-08-17 18:12:39 -0500

    Update elixir-mode--version

--- elixir-mode.el
+++ elixir-mode.el
@@ -140,7 +140,7 @@
 
 (require 'elixir-smie)  ; syntax and indentation support
 
-(defvar elixir-mode--version "1.3.0")
+(defvar elixir-mode--version "1.4.3")
 
 (defvar elixir-mode--website-url
   "http://elixir-lang.org")
