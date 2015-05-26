5394e0ff9816443f7429bc4ce779ae0c8d154419
Samuel Tonini <tonini.samuel@gmail.com>
2015-05-26 15:24:24 +0200

    bump version 2.2.3

--- elixir-mode.el
+++ elixir-mode.el
@@ -10,7 +10,7 @@
 ;; URL: https://github.com/elixir-lang/emacs-elixir
 ;; Created: Mon Nov 7 2011
 ;; Keywords: languages elixir
-;; Version: 2.2.2
+;; Version: 2.2.3
 
 ;; This file is not a part of GNU Emacs.
 
