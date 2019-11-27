e35b356ef2df0f419bd0436ea4224fa9aff93ad4
Dmitry Gutov <dgutov@yandex.ru>
2019-11-27 20:57:10 +0200

    Remove two more 'one-or-more' uses
    They never did anything anyway because 'identifiers' doesn't include
    any separators, so the resulting construct could only match one
    identifier anyway.

--- elixir-mode.el
+++ elixir-mode.el
@@ -304,7 +304,7 @@ is used to limit the scan."
     (elixir-match-interpolation 0 font-lock-variable-name-face t)
 
     ;; Module attributes
-    (,(elixir-rx (and "@" (1+ identifiers)))
+    (,(elixir-rx (and "@" identifiers))
      0 elixir-attribute-face)
 
     ;; Keywords
@@ -399,7 +399,7 @@ is used to limit the scan."
      1 font-lock-variable-name-face)
 
     ;; Map keys
-    (,(elixir-rx (group (and (one-or-more identifiers) ":")) space)
+    (,(elixir-rx (group (and identifiers ":")) space)
      1 elixir-atom-face)
 
     ;; Pseudovariables
