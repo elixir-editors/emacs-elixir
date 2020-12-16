9de08c17d54e190d220aa03cca58bd41497f44b6
Jonathan Arnett <jonarnett90@gmail.com>
2020-12-16 13:38:47 -0500

    Allow all sigils to be delimited by heredocs (#468)

--- elixir-mode.el
+++ elixir-mode.el
@@ -265,7 +265,7 @@ is used to limit the scan."
   (unless (elixir-syntax-in-string-or-comment-p)
     (let ((heredoc-p (save-excursion
                        (goto-char (match-beginning 0))
-                       (looking-at-p "~[sS]\\(?:'''\\|\"\"\"\\)"))))
+                       (looking-at-p "~[BCDELNRSTUbcersw]\\(?:'''\\|\"\"\"\\)"))))
       (unless heredoc-p
         (forward-char 1)
         (let* ((start-delim (char-after (1- (point))))
