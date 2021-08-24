8e0e8704c9ed7f464bb89939d6071fe46ffcd3f7
Thanabodee Charoenpiriyakij <wingyminus@gmail.com>
2021-08-24 18:55:53 +0700

    Enable elixir-mode by default when opening `mix.lock` file.
    Closes #480

--- elixir-mode.el
+++ elixir-mode.el
@@ -579,7 +579,8 @@ just return nil."
 (progn
   (add-to-list 'auto-mode-alist '("\\.elixir\\'" . elixir-mode))
   (add-to-list 'auto-mode-alist '("\\.ex\\'" . elixir-mode))
-  (add-to-list 'auto-mode-alist '("\\.exs\\'" . elixir-mode)))
+  (add-to-list 'auto-mode-alist '("\\.exs\\'" . elixir-mode))
+  (add-to-list 'auto-mode-alist '("mix\\.lock" . elixir-mode)))
 
 (provide 'elixir-mode)
 
