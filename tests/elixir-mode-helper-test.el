3ecc508f934be9e5ce16eb6e1b95c2a7eae999ce
Victor Oliveira Nascimento <victor.olinasc@gmail.com>
2022-03-13 20:16:47 -0300

    fix(tests): remove duplicated tests
    This breaks on Emacs master (as of currently: 29).

--- tests/elixir-mode-helper-test.el
+++ tests/elixir-mode-helper-test.el
@@ -17,7 +17,8 @@ defmodule Module.Name do
 
 end")
             (goto-line 7)
-            (elixir-smie--heredoc-at-current-point-p)))
+            (elixir-smie--heredoc-at-current-point-p)
+            ))
   (should (not (with-temp-buffer
                  (elixir-mode)
                  (insert "
@@ -44,8 +45,7 @@ defmodule Module.Name do
   end
 end")
                    (goto-line 4)
-                   (elixir-smie--previous-line-indentation))))
-  )
+                   (elixir-smie--previous-line-indentation)))))
 
 
 (ert-deftest check-if-previous-line-blank ()
@@ -72,82 +72,6 @@ defmodule Module.Name do
 end")
             (goto-line 4)
             (elixir-smie--previous-line-empty-p))))
-
-
-;;; elixir-mode-helper-test.el --- Tests for helper functions
-
-;;; Code:
-
-(ert-deftest check-if-currently-inside-heredoc ()
-  (should (with-temp-buffer
-            (elixir-mode)
-            (insert "
-defmodule Module.Name do
-
-  @moduledoc \"\"\"
-  ## Examples
-
-  ....
-  \"\"\"
-
-end")
-            (goto-line 7)
-            (elixir-smie--heredoc-at-current-point-p)))
-  (should (not (with-temp-buffer
-                 (elixir-mode)
-                 (insert "
-defmodule Module.Name do
-
-  @moduledoc \"\"\"
-  ## Examples
-
-  ....
-  \"\"\"
-
-end")
-                 (goto-line 3)
-                 (elixir-smie--heredoc-at-current-point-p)))))
-
-(ert-deftest get-previous-line-indentation ()
-  (should (equal 2
-                 (with-temp-buffer
-                   (elixir-mode)
-                   (insert "
-defmodule Module.Name do
-  def what do
-    1 + 1
-  end
-end")
-                   (goto-line 4)
-                   (elixir-smie--previous-line-indentation))))
-  )
-
-
-(ert-deftest check-if-previous-line-blank ()
-  (should (not (with-temp-buffer
-                 (elixir-mode)
-                 (insert "
-defmodule Module.Name do
-
-  def what do
-    1 + 1
-  end
-end")
-                 (goto-line 3)
-                 (elixir-smie--previous-line-empty-p))))
-  (should (with-temp-buffer
-            (elixir-mode)
-            (insert "
-defmodule Module.Name do
-
-
-  def what do
-    1 + 1
-  end
-end")
-            (goto-line 4)
-            (elixir-smie--previous-line-empty-p))))
-
 
 (ert-deftest test-current-line-contains-built-in-keyword ()
   (should (not (with-temp-buffer
